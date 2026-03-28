572dcd3e678f87c8c198a94e935ebf823858346c
YUNO <idzdi.adam@etu.uae.ac.ma>
2026-03-28 16:40:19 +0100

    Partie 6&7 student's CRUD et Authentication

--- school/faculty/views.py
+++ school/faculty/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
-    return render(request,'Home/index.html')
+from django.shortcuts import render, redirect 
+
+ 
+def index(request): 
+    return render(request, 'authentication/login.html')
+
+
+def dashboard(request): 
+    return render(request, 'students/student-dashboard.html') 
--- school/school/settings.py
+++ school/school/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'faculty',
     'student',
+    'home_auth',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),) 
+
+AUTH_USER_MODEL = 'home_auth.CustomUser' 
+AUTHENTICATION_BACKENDS = ( 
+    'django.contrib.auth.backends.ModelBackend',  # Backend par défaut 
+) 
+LOGIN_URL = '/authentication/login/' 
+LOGIN_REDIRECT_URL = '/dashboard/' 
